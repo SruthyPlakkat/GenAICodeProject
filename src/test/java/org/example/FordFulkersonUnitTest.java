b3267e71bbcabb7e279c9c6a42a1df231b453656
SruthyPlakkat <sruthy_plakkat@epam.com>
2024-11-15 13:43:25 +0530

    Add files via upload

---
+++ src/test/java/org/example/FordFulkersonUnitTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FordFulkersonUnitTest {
+    @Test
+    public void testFordFulkerson() {
+        FordFulkerson ff = new FordFulkerson();
+        int[][] graph = {
+                {0, 16, 13, 0, 0, 0},
+                {0, 0, 10, 12, 0, 0},
+                {0, 4, 0, 0, 14, 0},
+                {0, 0, 9, 0, 0, 20},
+                {0, 0, 0, 7, 0, 4},
+                {0, 0, 0, 0, 0, 0}
+        };
+        int result = ff.fordFulkerson(graph, 0, 5);
+        Assert.assertEquals(23, result);
+    }
+
+}
