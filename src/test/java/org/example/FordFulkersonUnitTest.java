ce6839a519fe51a9622e8358690a9d2fff5f20f5
SruthyPlakkat <sruthy_plakkat@epam.com>
2024-11-15 15:45:43 +0530

    Update FordFulkersonUnitTest.java

--- src/test/java/org/example/FordFulkersonUnitTest.java
+++ src/test/java/org/example/FordFulkersonUnitTest.java
@@ -4,10 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FordFulkersonUnitTest {
+    FordFulkerson ff = new FordFulkerson();
     @Test
     public void testFordFulkerson() {
-        FordFulkerson ff = new FordFulkerson();
-        int[][] graph = {
+                int[][] graph = {
                 {0, 16, 13, 0, 0, 0},
                 {0, 0, 10, 12, 0, 0},
                 {0, 4, 0, 0, 14, 0},
@@ -18,59 +18,6 @@ public class FordFulkersonUnitTest {
         int result = ff.fordFulkerson(graph, 0, 5);
         Assert.assertEquals(23, result);
     }
+}
 
-    @Test
-    public void testZeroCapacityEdges() {
-        FordFulkerson ff = new FordFulkerson();
-        int[][] graph = {
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0}
-        };
-        int result = ff.fordFulkerson(graph, 0, 5);
-        Assert.assertEquals(0, result);
-    }
-
-    @Test
-    public void testDisconnectedGraph() {
-        FordFulkerson ff = new FordFulkerson();
-        int[][] graph = {
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0}
-        };
-        int result = ff.fordFulkerson(graph, 0, 5);
-        Assert.assertEquals(0, result);
-    }
-
-    @Test
-    public void testMaximumCapacity() {
-        FordFulkerson ff = new FordFulkerson();
-        int[][] graph = {
-                {0, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 0, 0},
-                {0, 0, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 0},
-                {0, 0, 0, 0, Integer.MAX_VALUE, 0},
-                {0, 0, 0, 0, 0, Integer.MAX_VALUE},
-                {0, 0, 0, 0, 0, Integer.MAX_VALUE},
-                {0, 0, 0, 0, 0, 0}
-        };
-        int result = ff.fordFulkerson(graph, 0, 5);
-        Assert.assertEquals(Integer.MAX_VALUE, result);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testNegativeCapacities() {
-        FordFulkerson ff = new FordFulkerson();
-        int[][] graph = {
-                {0, -1, 0, 0, 0, 0},
-                {0, 0, -1, 0, 0, 0},
-                {0, 0, 0, -1, 0, 0},
-                {0, 0, 0, 0, -1, 0},
-                {0, 0, 0, 0, 0, -1},
-                {0, 0, 0, 0
+   
